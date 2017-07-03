5b14c722586184d6d0e5024e595a754bb76a12ef
Symon Rottem <s.rottem@symco.com.au>
2017-07-03 14:38:00 +0200

    Exposed error code on SovrinException.

--- wrappers/java/src/main/java/org/hyperledger/indy/sdk/SovrinException.java
+++ wrappers/java/src/main/java/org/hyperledger/indy/sdk/SovrinException.java
@@ -4,13 +4,19 @@ public class SovrinException extends Exception {
 
 	private static final long serialVersionUID = 2650355290834266477L;
 
-	public SovrinException(String message) {
-
-		super(message);
+	private ErrorCode errorCode;
+	
+	public SovrinException(String message, ErrorCode errorCode) {
+		super(message);		
+		this.errorCode = errorCode;
 	}
 
 	public static SovrinException fromErrorCode(ErrorCode errorCode, int err) {
 
-		return new SovrinException("" + (errorCode == null ? null : errorCode.name()) + ": " + (errorCode == null ? null : errorCode.value()) + " (" + Integer.toString(err) + ")");
+		return new SovrinException("" + (errorCode == null ? null : errorCode.name()) + ": " + (errorCode == null ? null : errorCode.value()) + " (" + Integer.toString(err) + ")", errorCode);
+	}
+	
+	public ErrorCode getErrorCode(){
+		return errorCode;
 	}
 }
